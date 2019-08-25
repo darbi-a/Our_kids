ffb00a1e56027d7e4ed2daef034a4f467c40c4d4
salehezz <saleh@itss-c.com>
2019-08-25 18:12:51 +0200

    Add new addons

---
+++ product_barcode_printout/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Barcode Print Out",
+
+    'summary': """
+        Product Barcode Print Out """,
+
+    'author': "ITSS , Mahmoud Naguib",
+    'website': "http://www.itss-c.com",
+
+    'category': 'Product',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['product'],
+
+    # always loaded
+    'data': [
+        'views/templates.xml',
+
+    ],
+
+
+
+}
