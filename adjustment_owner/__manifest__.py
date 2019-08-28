192530c11cf7468a40d4d82e1ef6fdb7b4f94f52
Mamdouh <mahmoud@itss-c.com>
2019-08-28 10:28:48 +0200

    [ADD] adjustment_owner

---
+++ adjustment_owner/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+#
+#
+
+{
+    "name": "Adjustment Owner",
+    "version": "12.2",
+    'license': 'GPL-3',
+    "author": "Ahmed Amin",
+    "category": "Inventory",
+    "depends": [
+        'stock',
+    ],
+    "demo": [],
+    "data": [
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
