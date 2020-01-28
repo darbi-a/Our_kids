525b9125b942ab7405ede66b84849ef0a96bad51
Mamdouh <mahmoud@itss-c.com>
2020-01-28 13:00:53 +0200

    [IMP] vendor_pricelist warehouse_restrictions

--- vendor_pricelist/models/pricelist.py
+++ vendor_pricelist/models/pricelist.py
@@ -34,6 +34,28 @@ class product_pricelist_item(models.Model):
         if self.applied_on != 'product_vendor':
             self.partner_id = False
 
+    @api.one
+    @api.depends('categ_id', 'product_tmpl_id','partner_id', 'product_id', 'compute_price', 'fixed_price','pricelist_id', 'percent_price', 'price_discount', 'price_surcharge')
+    def _get_pricelist_item_name_price(self):
+        if self.categ_id:
+            self.name = _("Category: %s") % (self.categ_id.name)
+        if self.partner_id:
+            self.name = _("Vendor: %s") % (self.partner_id.name)
+        elif self.product_tmpl_id:
+            self.name = self.product_tmpl_id.name
+        elif self.product_id:
+            self.name = self.product_id.display_name.replace('[%s]' % self.product_id.code, '')
+        else:
+            self.name = _("All Products")
+
+        if self.compute_price == 'fixed':
+            self.price = ("%s %s") % (self.fixed_price, self.pricelist_id.currency_id.name)
+        elif self.compute_price == 'percentage':
+            self.price = _("%s %% discount") % (self.percent_price)
+        else:
+            self.price = _("%s %% discount and %s surcharge") % (self.price_discount, self.price_surcharge)
+
+
 class product_pricelist(models.Model):
     _inherit = 'product.pricelist'
 
