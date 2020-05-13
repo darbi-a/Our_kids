7c7b7330a6cee1320eae7cf5bba0c36e26f424a8
mahmoud <mahmoud@itss-c.com>
2020-05-13 14:48:09 +0200

    [add] bus_delete_auto_vacum

---
+++ bus_delete_auto_vacum/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bus Delete Auto Vacum",
+
+    'summary': """
+        Bus Delete Auto Vacum """,
+
+    'author': "ITSS , Mahmoud Naguib",
+    'website': "http://www.itss-c.com",
+
+    'category': 'base',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['bus'],
+
+    # always loaded
+    'data': [
+
+    ],
+
+
+
+}
