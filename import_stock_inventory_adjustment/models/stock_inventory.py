8f4116213a7b3b464fc6f4323469888c7a6c36ce
mahmoud <mahmoud@itss-c.com>
2020-08-23 18:55:10 +0200

    [IMP] import_stock_inventory_adjustment

---
+++ import_stock_inventory_adjustment/models/stock_inventory.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+class StockInventory(models.Model):
+    _inherit = 'stock.inventory'
+
+    def post_inventory(self):
+        # The inventory is posted as a single step which means quants cannot be moved from an internal location to another using an inventory
+        # as they will be moved to inventory loss, and other quants will be created to the encoded quant location. This is a normal behavior
+        # as quants cannot be reuse from inventory location (users can still manually move the products before/after the inventory if they want).
+        self.mapped('move_ids').filtered(lambda move: move.state != 'done')[:2000]._action_done()
+        return True
